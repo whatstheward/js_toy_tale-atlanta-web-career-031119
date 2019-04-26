883a44533c3240224cc37908d960841147f42964
github username <github email address>
2019-04-26 11:06:01 -0400

    built cards, like buttons, and form handlers

---
+++ src/api.js
@@ -0,0 +1,46 @@
+const TOYS_URL = `http://localhost:3000/toys`
+
+function fetchToys(){
+    fetch(TOYS_URL)
+    .then(res => res.json())
+    .then(toys => toys.forEach(toy => handleToy(toy)))
+  }
+
+  function handleForm(e){
+    e.preventDefault()
+    let form = e.target 
+    
+    let newToy = {
+      name: form.name.value,
+      image: form.image.value,
+      likes: 0,
+    }
+    
+    handleToy(newToy)
+    
+    fetch(TOYS_URL, {
+      method:'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newToy)
+    })
+    
+    form.reset()
+    }
+    
+    function handleLike(e){
+      let toy = e.target.parentElement
+      toyId = toy.dataset.id
+      let likes = parseInt(toy.querySelector('p').innerHTML) + 1
+      console.log(likes)
+      fetch(`http://localhost:3000/toys/${toyId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+                 },
+        body:JSON.stringify({
+          'likes': likes
+             })
+          })
+    }
--- src/index.js
+++ src/index.js
@@ -2,18 +2,51 @@ const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
 
-// YOUR CODE HERE
+function handleToy(toy){
+  let collection = document.querySelector('#toy-collection')
+  let div = document.createElement('div')
+  div.className = 'card'
+  div.dataset.id = toy.id
+  
+  let img = document.createElement('img')
+  img.className = 'toy-avatar'
+  img.src = toy.image
+
+
+  let h2 = document.createElement('h2')
+  h2.innerText = toy.name
+
+  let p = document.createElement('p')
+  p.innerText = toy.likes
+
+  let btn = document.createElement('button')
+  btn.className = 'like-btn'
+  btn.innerText = 'Like!'
+  btn.addEventListener('click', handleLike)
+
+  div.appendChild(h2)
+  div.appendChild(img)
+  div.appendChild(p)
+  div.appendChild(btn)
+  collection.appendChild(div)
+  
+  }
 
 addBtn.addEventListener('click', () => {
-  // hide & seek with the form
   addToy = !addToy
   if (addToy) {
     toyForm.style.display = 'block'
-    // submit listener here
+    toyForm.addEventListener('submit', handleForm)
   } else {
     toyForm.style.display = 'none'
   }
 })
 
 
+
+
+
+
 // OR HERE!
+
+fetchToys()
